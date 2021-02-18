8269dd27d2ac666baf61dcc6323cede1079e0a83
minbr0ther <minbr0ther@naver.com>
2021-02-18 23:55:35 +0900

    21/02/18

---
+++ 6. Node js/node.js-mysql-1/lib/db.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'root',
+  database:'opentutorials'
+});
+db.connect();
+
+module.exports = db;
--- 6. Node js/node.js-mysql-1/lib/template.js
+++ 6. Node js/node.js-mysql-1/lib/template.js
@@ -9,6 +9,7 @@ module.exports = {
     </head>
     <body>
       <h1><a href="/">WEB</a></h1>
+      <a href="/author">author</a>
       ${list}
       ${control}
       ${body}
@@ -24,5 +25,37 @@ module.exports = {
     }
     list = list+'</ul>';
     return list;
+  },authorSelect:function(authors, author_id){
+    var tag = '';
+    var i = 0;
+    while(i < authors.length){
+      var selected = '';
+      if(authors[i].id === author_id) {
+        selected = ' selected';
+      }
+      tag += `<option value="${authors[i].id}"${selected}>${authors[i].name}</option>`;
+      i++;
+    }
+    return `
+      <select name="author">
+        ${tag}
+      </select>
+    `
+  },authorTable:function(authors){
+    var tag = '<table>';
+    var i = 0;
+    while(i < authors.length){
+        tag += `
+            <tr>
+                <td>${authors[i].name}</td>
+                <td>${authors[i].profile}</td>
+                <td>update</td>
+                <td>delete</td>
+            </tr>
+            `
+        i++;
+    }
+    tag += '</table>';
+    return tag;
   }
 }
