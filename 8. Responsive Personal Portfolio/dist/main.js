5fb8ad6f9c0f5b36b639c463f0aea6bb6513e334
minbr0ther <minbr0ther@naver.com>
2021-03-12 23:54:39 +0900

    21/03/12

--- 8. Responsive Personal Portfolio/dist/main.js
+++ 8. Responsive Personal Portfolio/dist/main.js
@@ -3,4 +3,34 @@ $(document).ready(function () {
     $(this).toggleClass("open");
     $(".top-nav").toggleClass("open");
   });
+
+  $(".top-nav .nav-link").on("click", function () {
+    $(".menu-toggler").removeClass("open");
+    $(".top-nav").removeClass("open");
+  });
+
+  $('nav a[href*="#"]').on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - 100,
+      },
+      2000
+    );
+  });
+
+  $("#up").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500
+    );
+  });
+
+  AOS.init({
+    easing: "ease",
+    duration: 1800,
+    once: true,
+    // 한번만 실행하게 함
+  });
 });
