47f421dfa2bfdacd6d590537be4c370796361645
minbr0ther <minbr0ther@naver.com>
2021-04-08 00:26:59 +0900

    21/04/08

--- 9. React/management by DongbinNa/client/src/App.js
+++ 9. React/management by DongbinNa/client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = (theme) => ({
@@ -19,14 +20,20 @@ const styles = (theme) => ({
     minWidth: 1080,
     // 테이블의 최소크기를 정해서 테이블의 모양이 망가지지 않게 한다
   },
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
 });
 
 class App extends Component {
   state = {
     customers: "",
+    completed: 0,
+    // 프로그레스 바 게이지가 0
   };
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20); //0.02초 마다 progress 실행
     this.callApi()
       .then((res) => this.setState({ customers: res }))
       .catch((err) => console.log(err));
@@ -36,6 +43,12 @@ class App extends Component {
     const response = await fetch("api/customers");
     const body = await response.json();
     return body;
+  };
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+    //결과적으로 0부터 100까지 계속 왔다갔다 하게됨
   };
 
   render() {
@@ -54,20 +67,33 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map((c) => {
-              return (
-                <Customer
-                  key={c.id}
-                  //map 함수는 key값을 요구한다
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            })}
+            {this.state.customers ? (
+              this.state.customers.map((c) => {
+                return (
+                  <Customer
+                    key={c.id}
+                    //map 함수는 key값을 요구한다
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                );
+              })
+            ) : (
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress
+                    ClassName={classes.progress}
+                    variant="determinate"
+                    value={this.state.completed}
+                  />
+                </TableCell>
+              </TableRow>
+            )}
+            {/* 고객을 받아오는데 시간이 걸리기 때문에 처음엔 공백으로 보여줌 */}
           </TableBody>
         </Table>
       </Paper>
--- 9. React/management by DongbinNa/server.js
+++ 9. React/management by DongbinNa/server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,33 +7,23 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.por,
+  database: conf.database,
+});
+connection.connect();
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "홍길동",
-      birthday: "951229",
-      gender: "여자",
-      job: "대학생",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "나동빈",
-      birthday: "951229",
-      gender: "남자",
-      job: "군인",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "정민형",
-      birthday: "951229",
-      gender: "남자",
-      job: "개발자",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
