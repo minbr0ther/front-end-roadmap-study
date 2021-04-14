97e5cd42a9ecae05afd899f9b63ccdc8933e584e
minbr0ther <minbr0ther@naver.com>
2021-04-15 00:20:58 +0900

    21/04/14

--- 9. React/react-basic by DreamCoding/habit-tracker/src/app.jsx
+++ 9. React/react-basic by DreamCoding/habit-tracker/src/app.jsx
@@ -1,7 +1,8 @@
 import "./app.css";
+import Habit from "./components/habit";
 
 function App() {
-  return <h1>Hello React</h1>;
+  return <Habit></Habit>;
 }
 
 export default App;
---
+++ 9. React/react-basic by DreamCoding/habit-tracker/src/components/habit.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    // state 오브젝트에 있는 count를 증가 한뒤 state를 업데이트 해야 함
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
+  render() {
+    return (
+      <li className="habit">
+        <span className="habit-name">Reading</span>
+        <span className="habit-count">{this.state.count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button className="habit-button habit-delete">
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
+    );
+  }
+}
+
+export default Habit;
