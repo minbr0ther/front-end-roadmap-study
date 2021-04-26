a29a948baffe8dfae4dd4ca87be6588fdd0a6ae5
minbr0ther <minbr0ther@naver.com>
2021-04-26 23:59:46 +0900

    21/04/26

---
+++ 9. React/react-basic by DreamCoding/habit-tracker-2/src/app.jsx
@@ -0,0 +1,8 @@
+import "./app.css";
+import Habits from "./components/habits";
+
+function App() {
+  return <Habits />;
+}
+
+export default App;
---
+++ 9. React/react-basic by DreamCoding/habit-tracker-2/src/components/habit.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+class Habit extends Component {
+  handleIncreament = () => {
+    //state 오브젝트 안에 인ㅆ는 count를 증가 한뒤 state를 업데이트 해야 함
+    //this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit);
+  };
+
+  handleDecreament = () => {
+    // const count = this.state.count - 1;
+    // this.setState({ count: count < 0 ? 0 : count });
+    //count가 0이하면 0을 출력하고 아니면 count 출력
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+
+  render() {
+    const { name, count } = this.props.habit;
+    //const name = this.props.habit.name;
+    //const count = this.props.habit.count;
+
+    return (
+      <li className="habit">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncreament}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecreament}
+        >
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
+    );
+  }
+}
+
+export default Habit;
