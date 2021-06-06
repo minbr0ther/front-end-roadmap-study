120eb464bd8c65c13c86d7282054e3d9b2420eab
minbr0ther <minbr0ther@naver.com>
2021-06-06 22:55:32 +0900

    21/06/06

--- 9. React/react-basic by DreamCoding/card-maker-2/src/components/editor/editor.jsx
+++ 9. React/react-basic by DreamCoding/card-maker-2/src/components/editor/editor.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import CardEditForm from "../card_edit_form/card_edit_form";
 import styles from "./editor.module.css";
 
-const Editor = (props) => (
+const Editor = ({ cards }) => (
   <section className={styles.editor}>
     <h1 className={styles.title}>Card Maker</h1>
     {cards.map((card) => (
-      <Card card={card}></Card>
+      <CardEditForm card={card} />
     ))}
   </section>
 );
